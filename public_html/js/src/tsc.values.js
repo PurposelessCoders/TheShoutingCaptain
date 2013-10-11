9ed3c8c8bb96cfee024e677a073bcf929de7772f
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-10-11 17:30:09 +0200

    adding namespace and define WATER_FRICTION

---
+++ public_html/js/src/tsc.values.js
@@ -0,0 +1,6 @@
+(function () {
+  /**
+   * @namespace values
+   */
+  tsc.values = {};
+}).call(this);
---
+++ public_html/js/src/tsc.values.physic.js
@@ -0,0 +1,12 @@
+(function () {
+  /**
+   * @namespace physic
+   */
+  var physic = tsc.values.physic = {};
+
+  /**
+   * @constant
+   */
+  physic.WATER_FRICTION = 1;
+
+}).call(this);
