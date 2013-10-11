c02f98982271404b824bbb61a76ec3a2c18ab250
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-10-11 22:13:06 +0200

    handle keyboard events on program loop

--- public_html/js/src/tsc.main.js
+++ public_html/js/src/tsc.main.js
@@ -2,7 +2,9 @@
 
   var DrawerCanvas = tsc.drawer.DrawerCanvas;
   var Ship = tsc.Ship;
+  var ShipDelegate = tsc.ShipDelegate;
   var display = tsc.values.display;
+  var Keyboard = tsc.input.Keyboard;
 
   window.onload = function () {
     var canvas = document.getElementById("gameCanvas");
@@ -12,6 +14,12 @@
     var midX = canvas.width / 2;
     var midY = canvas.height / 2;
     var ship = new Ship(midX, midY, 0);
+    var shipDelegate = new ShipDelegate(ship);
+    var input = new Keyboard(canvas);
+
+    input.registerEventHandler(function (key) {
+      shipDelegate.setKey(key);
+    });
 
     drawer.addShip(ship);
     drawer.drawShip();
@@ -19,6 +27,7 @@
     var timeFrameMiliSec = 1000 / display.FRAME_RATE;
     var timeFrameSec = 1 / display.FRAME_RATE;
     setInterval(function () {
+      shipDelegate.action();
       ship.move(timeFrameSec);
       drawer.drawShip();
     }, timeFrameMiliSec);
