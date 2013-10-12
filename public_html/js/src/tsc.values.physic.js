58b7b3fb4c86381698b6e37ade058e16403220a9
Cyril Coutelier <cyril.coutelier@epitech.eu>
2013-10-12 02:40:51 +0200

    add physics constants

--- public_html/js/src/tsc.values.physic.js
+++ public_html/js/src/tsc.values.physic.js
@@ -8,5 +8,20 @@
    * @constant
    */
   physic.WATER_FRICTION = .05;
+  
+  /**
+   * @constant
+   */
+  physic.MAX_SPEED = 4;
+  
+  /**
+   * @constant
+   */
+  physic.ACCELERATION = .5;
+  
+  /**
+   * @constant
+   */
+  physic.DECELERATION = .25;
 
 }).call(this);
