a815540a9055298837ca506ab563fb60140308b6
Armand Besombes <armand.besombes@epitech.net>
2013-10-14 04:52:50 +0200

    wind impact on the ship acceleration and speed

--- public_html/js/src/tsc.Ship.js
+++ public_html/js/src/tsc.Ship.js
@@ -3,7 +3,7 @@
   var physic = tsc.values.physic;
 
   var ANGULAR_SPEED = 5;
-
+  var WIND_POWER_COEF = 0.5;
   /**
    * @constructor
    * @param {type} posX
@@ -33,13 +33,29 @@
   _ship.stopTurning = function () {
     this.angularSpeed = 0;
   };
+  
+  _ship.speedCoef = function () {
+      var myAngle = (this.angle + 90) % 360;      
+      var angleA = Math.min(myAngle, tsc.global.wind.getAngle());
+      var angleB = Math.max(myAngle, tsc.global.wind.getAngle());
 
+      diffAngle = Math.min(Math.abs(tsc.global.wind.getAngle() - myAngle),
+                           Math.abs(360 - angleB + angleA));
+
+      return ((180 - diffAngle) / 180);
+  };
+  
   _ship.accelerate = function () {
-    if (this.linearSpeed < physic.MAX_SPEED - physic.ACCELERATION) {
-      this.linearSpeed += physic.ACCELERATION;
-    } else {
-      this.linearSpeed = physic.MAX_SPEED;
-    }
+      var coef = this.speedCoef();
+
+      if (this.linearSpeed < physic.MAX_SPEED * coef) {
+          var clc = physic.ACCELERATION * coef * tsc.global.wind.getPower() * WIND_POWER_COEF;
+          if (this.linearSpeed + clc >= physic.MAX_SPEED * coef)
+              this.linearSpeed = physic.MAX_SPEED * coef;
+          else
+              this.linearSpeed += physic.ACCELERATION * coef * tsc.global.wind.getPower() * WIND_POWER_COEF;
+      }  else if (this.linearSpeed > physic.MAX_SPEED * coef)
+            this.stopAccelerate();
   };
 
   _ship.decelerate = function () {
@@ -65,6 +81,10 @@
 
   _ship.moveAngular = function (time) {
     this.angle += this.angularSpeed;
+    if (this.angle > 360)
+        this.angle = this.angle % 360;
+    else if (this.angle < 0)
+        this.angle = 360 - this.angle;
   };
 
   _ship.moveLinear = function () {
