5eb7f0b40a2e125581c378e0d9e0cf471c73c590
Armand Besombes <armand.besombes@epitech.net>
2013-10-24 17:20:23 +0200

    Add moral and hp in the crew

--- public_html/js/src/tsc.shipComponents.crew.js
+++ public_html/js/src/tsc.shipComponents.crew.js
@@ -5,9 +5,40 @@
     var Crew = function (size) {
         this.size = size;
         this.hp = size * HP_BY_MEMBERS;
+        this.bonus = 50;
+        this.bonuTimer = 0;
     };
     
     tsc.shipComponents.Crew = Crew;
     
     var _crew = Crew.prototype;
+    
+    _crew.hpDamages = function (dm) {
+        this.hp -= dm;
+        
+        var crewSize = this.hp / HP_BY_MEMBERS;
+        if (crewSize < this.size) {
+            this.moralMalus(5 * (this.size - crewSize));
+            this.size = crewSize;
+        }
+           
+        if (this.hp <= 0)
+            //LOSER
+    };
+    
+    _crew.moralBonus = function (bonus) {
+        this.bonus = Math.max(0, Math.min(100, this.bonus + bonus));
+        this.bonusTimer = new Date().getTime();
+    };
+    
+    _crew.moralMalus = function (malus) {
+        this.moralBonus(-malus);
+    };
+    
+    _crew.moralInTime = function () {
+        this.bonusTimer -= tsc.values.time.DeltaTime();
+        if (this.bonusTimer <= 0) {
+            this.moralMalus(25);
+        }
+    };
 }).call(this);
